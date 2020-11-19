4f8497dd99c7ea61bdbbec106588a705fc7c5437
Ayush Rajniwal <ayush.rajniwal@joshtechnologygroup.com>
2020-11-19 11:46:37 +0530

    [RA-02] User data save in redux store

--- src/app/redux/reducer.js
+++ src/app/redux/reducer.js
@@ -1,6 +1,7 @@
 import initialState from "./store";
 
 function reducer(state = initialState, action) {
+    let payload = action.payload;
     switch (action.type) {
         case "TOGGLE_NAV":
             return {
@@ -12,6 +13,22 @@ function reducer(state = initialState, action) {
             return {
                 ...state,
                 isLoggedIn: !state.isLoggedIn,
+            };
+
+        case "SAVE_USER":
+            return {
+                ...state,
+                profile: {
+                    avatar: payload.data.avatar,
+                    username: payload.data.login,
+                    location: payload.data.location,
+                    following: payload.data.following,
+                    followers: payload.data.followers,
+                    bio: payload.data.bio,
+                    link: payload.data.html_url,
+                    blog: payload.data.blog,
+                    email: payload.data.email,
+                },
             };
         default:
             return state;
