a9f5c51b22145e5792de5f6f11ee4a09bf7612e9
Ayush Rajniwal <ayush.rajniwal@gmail.com>
2020-11-19 11:16:20 +0530

    [RA-02] Story created for Popup component

---
+++ src/stories/Popup.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Popup from "../app/components/Popup";
+
+export default {
+    title: "Popup",
+    component: Popup,
+};
+
+export const PopupDisplay = () => <Popup title="Title" message="Message" />;
