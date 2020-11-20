b90ac33489393e85e7c6c52d9f4f4fa2ea2a9c50
Ayush Rajniwal <ayush.rajniwal@joshtechnologygroup.com>
2020-11-20 14:29:28 +0530

    [RA-03] Stories created Avarat, IconText, Profile stories added text--large class added to util min width remove from profile__container on small screen

---
+++ src/stories/Avatar.stories.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Avatar from "../app/components/Avatar";
+
+export default {
+    title: "Avatar",
+    component: Avatar,
+    decorators: [
+        (story) => <div className="profile__img-wrapper">{story()}</div>,
+    ],
+};
+
+export const AvatarDisplay = () => (
+    <Avatar img="https://avatars3.githubusercontent.com/u/54205?s=460&v=4" />
+);
---
+++ src/stories/Profile.stories.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Profile from "../app/components/Profile";
+
+export default {
+    title: "Profile",
+    component: Profile,
+};
+
+export const ProfileCard = () => (
+    <Profile
+        profile={{
+            avatar: "https://avatars3.githubusercontent.com/u/54205?s=460&v=4",
+            link: "https://github.com/Ayush-Rajniwal/",
+            username: "username",
+            bio: "Bio will appear here",
+            followers: "22",
+            following: "32",
+            location: "location",
+            name: "Name Will Appear here",
+            email: "email",
+            blog: "blog",
+        }}
+    />
+);
