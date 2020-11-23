c9a243970fdf18e39101ad24395c0bb520fec7eb
Ayush Rajniwal <ayush.rajniwal@gmail.com>
2020-11-23 11:06:19 +0530

    [RA-05] Adding Story for List and SearchBar Correcting font-size of list component

---
+++ src/stories/SearchBar.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import SearchBar from "../app/components/SearchBar";
+
+export default {
+    title: "SearchBar",
+    component: SearchBar,
+};
+
+export const Search = () => <SearchBar onChange="" />;
