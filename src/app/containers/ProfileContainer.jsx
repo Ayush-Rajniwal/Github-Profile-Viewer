9c0de7ec4c59b4913bf5f3b777c6f24aa3730cc9
Ayush Rajniwal <ayush.rajniwal@joshtechnologygroup.com>
2020-11-28 00:05:11 +0530

    [RA-03] apiCall modified to handle PUT request

--- src/app/containers/ProfileContainer.jsx
+++ src/app/containers/ProfileContainer.jsx
@@ -1,13 +1,12 @@
 import React, { useEffect, useState, useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import axios from 'axios';
 import { useTranslation } from 'react-i18next';
 import Profile from '@components/Profile';
 import Popup from '@components/Popup';
 import List from '@components/List';
 import NotFound from '@components/NotFound';
 import PropTypes from 'prop-types';
-import GET from '@services/apiCall';
+import apiCall from '@services/apiCall';
 
 function ProfileContainer({
     match: {
@@ -26,7 +25,10 @@ function ProfileContainer({
     const [followingList, setFollowingList] = useState([]);
 
     const fetchUser = useCallback(() => {
-        GET(`/users/${username}`, { isAuthenticated: isLoggedIn, password: loggedInUser.token })
+        apiCall('GET', `/users/${username}`, {
+            isAuthenticated: isLoggedIn,
+            password: loggedInUser.token,
+        })
             .then((response) => {
                 dispatch({
                     type: 'SAVE_USER',
@@ -39,7 +41,7 @@ function ProfileContainer({
     }, [dispatch, isLoggedIn, loggedInUser.token, username]);
 
     const fetchFollowers = useCallback(() => {
-        GET(`/users/${username}/followers?per_page=5`, {
+        apiCall('GET', `/users/${username}/followers?per_page=5`, {
             isAuthenticated: isLoggedIn,
             password: loggedInUser.token,
         })
@@ -57,7 +59,7 @@ function ProfileContainer({
     }, [username, isLoggedIn, loggedInUser.token]);
 
     const fetchFollowing = useCallback(() => {
-        GET(`/users/${username}/following?per_page=5`, {
+        apiCall('GET', `/users/${username}/following?per_page=5`, {
             isAuthenticated: isLoggedIn,
             password: loggedInUser.token,
         })
@@ -84,20 +86,14 @@ function ProfileContainer({
 
     const followBtn = (e) => {
         const { id } = e.target.dataset;
-        const config = {
-            method: 'PUT',
-            url: `https://api.github.com/user/following/${id}`,
-            headers: {
-                Authorization: `token ${loggedInUser.token}`,
-            },
-        };
-        axios(config)
-            .then((response) => {
-                console.log(response);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
+
+        apiCall('PUT', `/user/following/${id}`, {
+            isAuthenticated: isLoggedIn,
+            password: loggedInUser.token,
+        }).then(() => {
+        }).catch(() => {
+            setNoProfile(true);
+        });
     };
 
     const toggleFollowerList = () => {
