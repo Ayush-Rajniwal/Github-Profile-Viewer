3da5c06924f5d5d54b06cb237709bf4bcb669c46
Ayush Rajniwal <ayush.rajniwal@gmail.com>
2020-11-28 11:06:47 +0530

    [RA-03] Review Fixes Loading spinner added text clipped for location value

--- src/app/redux/actionTypes.js
+++ src/app/redux/actionTypes.js
@@ -2,3 +2,5 @@ export const TOGGLE_NAV = 'TOGGLE_NAV';
 export const LOGIN_USER = 'LOGIN_USER';
 export const SAVE_USER = 'SAVE_USER';
 export const LOGOUT_USER = 'LOGOUT_USER';
+export const START_LOADING = 'START_LOADING';
+export const STOP_LOADING = 'STOP_LOADING';
