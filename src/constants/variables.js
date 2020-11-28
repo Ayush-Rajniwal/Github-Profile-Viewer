49da9db74cad1c419fd9bf33b9d350e6c46e6328
Ayush Rajniwal <ayush.rajniwal@gmail.com>
2020-11-28 15:52:52 +0530

    [RA-04] static URLs are moved to constants tranlation key change

--- src/constants/variables.js
+++ src/constants/variables.js
@@ -3,3 +3,5 @@ export const SUBMIT = 'submit';
 export const API_URL = 'https://api.github.com';
 export const FOLLOW = 'Follow';
 export const REMOVE = 'Remove';
+export const FOLLOWING_URL = '/user/following';
+export const USERS_URL = '/users';
